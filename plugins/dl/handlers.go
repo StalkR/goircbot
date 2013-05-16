4623ac83b740da90813edec4e43873cccd578546
StalkR <github@stalkr.net>
2013-05-16 23:14:02 +0200

    plugins: dl: add download

--- plugins/dl/handlers.go
+++ plugins/dl/handlers.go
@@ -3,52 +3,88 @@ package dl
 
 import (
 	"log"
+	"regexp"
+	"strings"
 
 	"github.com/StalkR/goircbot/bot"
 	"github.com/StalkR/goircbot/lib/mldonkey"
 	"github.com/StalkR/goircbot/lib/transmission"
 )
 
-func mldonkeyStats(url string) (string, error) {
-	s, err := mldonkey.Stats(url)
-	if err != nil {
-		return "", nil
-	}
-	return s.String(), nil
-}
+var (
+	transmissionRE = regexp.MustCompile(`^(https?://|magnet:?)`)
+	mldonkeyRE     = regexp.MustCompile(`^ed2k://`)
+)
 
-func transmissionStats(url string) (string, error) {
-	s, err := transmission.Stats(url)
+func handleMLDonkey(b *bot.Bot, e *bot.Event, url string) {
+	c, err := mldonkey.New(url)
 	if err != nil {
-		return "", nil
+		log.Println("dl: mldonkey new:", err)
+		return
 	}
-	return s.String(), nil
-}
-
-func dl(b *bot.Bot, e *bot.Event, mldonkeyURL, transmissionURL string) {
-	if mldonkeyURL != "" {
-		if stats, err := mldonkeyStats(mldonkeyURL); err != nil {
+	args := strings.SplitN(strings.TrimSpace(e.Args), " ", 2)
+	link := args[0]
+	if link == "" {
+		stats, err := c.Stats()
+		if err != nil {
 			log.Println("dl: mldonkey:", err)
-		} else {
-			b.Conn.Privmsg(e.Target, "MLDonkey: "+stats)
+			return
 		}
+		b.Conn.Privmsg(e.Target, "[MLDonkey] "+stats.String())
+		return
 	}
-	if transmissionURL != "" {
-		if stats, err := transmissionStats(transmissionURL); err != nil {
-			log.Println("dl: transmission:", err)
-		} else {
-			b.Conn.Privmsg(e.Target, "Transmission: "+stats)
+	if !mldonkeyRE.MatchString(link) {
+		return
+	}
+	if err := c.Add(link); err != nil {
+		log.Println("dl: mldonkey add:", err)
+		return
+	}
+	b.Conn.Privmsg(e.Target, "[MLDonkey] Added")
+}
+
+func handleTransmission(b *bot.Bot, e *bot.Event, url string) {
+	c, err := transmission.New(url)
+	if err != nil {
+		log.Println("dl: transmission new:", err)
+		return
+	}
+	args := strings.SplitN(strings.TrimSpace(e.Args), " ", 2)
+	link := args[0]
+	if link == "" {
+		stats, err := c.Stats()
+		if err != nil {
+			log.Println("dl: transmission stats:", err)
+			return
 		}
+		b.Conn.Privmsg(e.Target, "[Transmission] "+stats.String())
+		return
 	}
+	if !transmissionRE.MatchString(link) {
+		return
+	}
+	name, err := c.Add(link)
+	if err != nil {
+		log.Println("dl: transmission add:", err)
+		return
+	}
+	b.Conn.Privmsg(e.Target, "[Transmission] Added: "+name)
 }
 
 // Register registers the plugin with a bot.
 func Register(b *bot.Bot, mldonkeyURL, transmissionURL string) {
 
 	b.AddCommand("dl", bot.Command{
-		Help:    "See downloads status",
-		Handler: func(b *bot.Bot, e *bot.Event) { dl(b, e, mldonkeyURL, transmissionURL) },
-		Pub:     true,
-		Priv:    true,
-		Hidden:  false})
+		Help: "See downloads status or add downloads",
+		Handler: func(b *bot.Bot, e *bot.Event) {
+			if mldonkeyURL != "" {
+				handleMLDonkey(b, e, mldonkeyURL)
+			}
+			if transmissionURL != "" {
+				handleTransmission(b, e, transmissionURL)
+			}
+		},
+		Pub:    true,
+		Priv:   false,
+		Hidden: false})
 }
