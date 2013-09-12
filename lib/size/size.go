ae2375e66e5ee2057562892cbb6569adcb0c2126
StalkR <github@stalkr.net>
2013-09-12 16:46:01 +0200

    lib/size: fmt float with 3 significant digits

--- lib/size/size.go
+++ lib/size/size.go
@@ -13,20 +13,35 @@ const (
 	GB
 	TB
 	PB
+	EB
 )
 
+// String represents a Byte with the most appropriate unit.
 func (b Byte) String() string {
 	switch {
+	case b >= EB:
+		return fmt.Sprintf("%vEB", b.fmt(EB))
 	case b >= PB:
-		return fmt.Sprintf("%dPB", b/PB)
+		return fmt.Sprintf("%vPB", b.fmt(PB))
 	case b >= TB:
-		return fmt.Sprintf("%dTB", b/TB)
+		return fmt.Sprintf("%vTB", b.fmt(TB))
 	case b >= GB:
-		return fmt.Sprintf("%dGB", b/GB)
+		return fmt.Sprintf("%vGB", b.fmt(GB))
 	case b >= MB:
-		return fmt.Sprintf("%dMB", b/MB)
+		return fmt.Sprintf("%vMB", b.fmt(MB))
 	case b >= KB:
-		return fmt.Sprintf("%dKB", b/KB)
+		return fmt.Sprintf("%vKB", b.fmt(KB))
 	}
-	return fmt.Sprintf("%dB", b)
+	return fmt.Sprintf("%vB", b)
+}
+
+// fmt represents a Byte in a unit with 3 significant digits minimum.
+func (b Byte) fmt(unit Byte) string {
+	f := "%.0f"
+	if b < 10*unit {
+		f = "%.2f"
+	} else if b < 100*unit {
+		f = "%.1f"
+	}
+	return fmt.Sprintf(f, float64(b)/float64(unit))
 }
