69701a3067bc05ba64dfb26a7ed5fd5b340c4046
StalkR <github@stalkr.net>
2013-11-28 18:20:01 +0100

    plugins/df: fix linux

--- plugins/df/space_linux.go
+++ plugins/df/space_linux.go
@@ -9,7 +9,7 @@ func space(path string) (total, free int, err error) {
 	s := syscall.Statfs_t{}
 	err = syscall.Statfs(path, &s)
 	if err != nil {
-		return err
+		return
 	}
 	total = int(s.Bsize) * int(s.Blocks)
 	free = int(s.Bsize) * int(s.Bfree)
