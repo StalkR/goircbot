10513518f9141169c259e6afbddd8911aff63cf4
StalkR <stalkr@stalkr.net>
2013-11-30 14:53:07 +0100

    plugins/urban: rewrite with new bot

--- plugins/urban/handlers.go
+++ plugins/urban/handlers.go
@@ -9,22 +9,22 @@ import (
 	"github.com/StalkR/goircbot/lib/urbandictionary"
 )
 
-func define(b *bot.Bot, e *bot.Event) {
+func define(e *bot.Event) {
 	term := strings.TrimSpace(e.Args)
 	if len(term) == 0 {
 		return
 	}
 	r, err := urbandictionary.Define(term)
 	if err != nil {
-		b.Conn.Privmsg(e.Target, fmt.Sprintf("error: %s", err))
+		e.Bot.Privmsg(e.Target, fmt.Sprintf("error: %s", err))
 		return
 	}
-	b.Conn.Privmsg(e.Target, r.String())
+	e.Bot.Privmsg(e.Target, r.String())
 }
 
 // Register registers the plugin with a bot.
-func Register(b *bot.Bot) {
-	b.AddCommand("urban", bot.Command{
+func Register(b bot.Bot) {
+	b.Commands().Add("urban", bot.Command{
 		Help:    "get definition of word from Urban Dictionary",
 		Handler: define,
 		Pub:     true,
