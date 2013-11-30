b5eb62290d762350da69ad5fdfa7a8bc0b2682d7
StalkR <stalkr@stalkr.net>
2013-11-30 14:22:15 +0100

    plugins/imdb: rewrite with new bot

--- plugins/imdb/handlers.go
+++ plugins/imdb/handlers.go
@@ -10,36 +10,36 @@ import (
 	imdb "github.com/StalkR/imdb/appengine"
 )
 
-func search(b *bot.Bot, e *bot.Event) {
+func search(e *bot.Event) {
 	q := strings.TrimSpace(e.Args)
 	if len(q) == 0 {
 		return
 	}
 	c, err := transport.Client(imdb.AppURL)
 	if err != nil {
-		b.Conn.Privmsg(e.Target, fmt.Sprintf("error: %s", err))
+		e.Bot.Privmsg(e.Target, fmt.Sprintf("error: %s", err))
 		return
 	}
 	titles, err := imdb.SearchTitle(c, q)
 	if err != nil {
-		b.Conn.Privmsg(e.Target, fmt.Sprintf("error: %s", err))
+		e.Bot.Privmsg(e.Target, fmt.Sprintf("error: %s", err))
 		return
 	}
 	if len(titles) == 0 {
-		b.Conn.Privmsg(e.Target, "No results found.")
+		e.Bot.Privmsg(e.Target, "No results found.")
 		return
 	}
 	title, err := imdb.NewTitle(c, titles[0].ID)
 	if err != nil {
-		b.Conn.Privmsg(e.Target, fmt.Sprintf("error: %s", err))
+		e.Bot.Privmsg(e.Target, fmt.Sprintf("error: %s", err))
 		return
 	}
-	b.Conn.Privmsg(e.Target, title.String())
+	e.Bot.Privmsg(e.Target, title.String())
 }
 
 // Register registers the plugin with a bot.
-func Register(b *bot.Bot) {
-	b.AddCommand("imdb", bot.Command{
+func Register(b bot.Bot) {
+	b.Commands().Add("imdb", bot.Command{
 		Help:    "imdb <title> - search a Title on IMDb",
 		Handler: search,
 		Pub:     true,
