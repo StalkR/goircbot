59b78be776807c9080f457974515665f1cf1b9ec
StalkR <stalkr@stalkr.net>
2013-11-30 14:48:27 +0100

    plugins/translate: rewrite with new bot

--- plugins/translate/handlers.go
+++ plugins/translate/handlers.go
@@ -31,7 +31,7 @@ func compactSpaces(s string) string {
 	return string(r.ReplaceAll([]byte(s), []byte(" ")))
 }
 
-func tr(b *bot.Bot, e *bot.Event, key string) {
+func tr(e *bot.Event, key string) {
 	line := strings.TrimSpace(compactSpaces(e.Args))
 	args := strings.SplitN(line, " ", 3)
 	var reply string
@@ -73,14 +73,14 @@ func tr(b *bot.Bot, e *bot.Event, key string) {
 	if len(reply) > 300 {
 		reply = reply[:300]
 	}
-	b.Conn.Privmsg(e.Target, reply)
+	e.Bot.Privmsg(e.Target, reply)
 }
 
 // Register registers the plugin with a bot.
-func Register(b *bot.Bot, key string) {
-	b.AddCommand("tr", bot.Command{
+func Register(b bot.Bot, key string) {
+	b.Commands().Add("tr", bot.Command{
 		Help:    "translate <from|-> <to> <text> using Google Translate",
-		Handler: func(b *bot.Bot, e *bot.Event) { tr(b, e, key) },
+		Handler: func(e *bot.Event) { tr(e, key) },
 		Pub:     true,
 		Priv:    true,
 		Hidden:  false})
