ab6602b4ff88699e7b996423ec4f0843b5f3717a
StalkR <github@stalkr.net>
2015-06-11 15:15:02 +0300

    plugins/asm: generate: sort before printing the map (more predictable and less diff if changes)

--- plugins/asm/generate/mnemonics.go
+++ plugins/asm/generate/mnemonics.go
@@ -10,42 +10,58 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sort"
 )
 
 const xmlURL = "http://ref.x86asm.net/x86reference.xml"
 
 var (
-	pkg     = flag.String("p", "", "Package name.")
+	pkg     = flag.String("p", "asm", "Package name.")
 	varname = flag.String("v", "mnemonics", "Variable name.")
-	out     = flag.String("o", "", "Output file.")
+	out     = flag.String("o", "mnemonics.go", "Output file.")
 )
 
 func main() {
 	flag.Parse()
-	resp, err := http.Get(xmlURL)
+	mnemonics, err := Get()
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	page, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var x xmlRef
-	if err := xml.Unmarshal(page, &x); err != nil {
 		log.Fatal(err)
 	}
 	var b bytes.Buffer
 	fmt.Fprintf(&b, "package %s\n", *pkg)
 	b.WriteString("\n")
 	fmt.Fprintf(&b, "var %s = map[string]string{\n", *varname)
-	for mnem, brief := range x.Mnemonics() {
-		fmt.Fprintf(&b, "\t%#v: %#v,\n", mnem, brief)
+	for _, m := range mnemonics {
+		fmt.Fprintf(&b, "\t%#v: %#v,\n", m.Name, m.Brief)
 	}
 	b.WriteString("}\n")
 	if err := ioutil.WriteFile(*out, b.Bytes(), 0660); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Get obtains the XML reference, parses it and returns a slice of mnemonics
+// sorted by name and with a brief description.
+func Get() ([]Mnemonic, error) {
+	resp, err := http.Get(xmlURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	page, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var x xmlRef
+	if err := xml.Unmarshal(page, &x); err != nil {
+		return nil, err
+	}
+	var mnemonics []Mnemonic
+	for name, brief := range x.Mnemonics() {
+		mnemonics = append(mnemonics, Mnemonic{Name: name, Brief: brief})
+	}
+	sort.Sort(byName(mnemonics))
+	return mnemonics, nil
 }
 
 // An xmlRef represents the partially-parsed XML reference.
@@ -72,7 +88,7 @@ type syntax struct {
 }
 
 func (r *xmlRef) Mnemonics() map[string]string {
-	m := make(map[string]string)
+	set := make(map[string]string)
 	for _, a := range []byteEl{r.OneByte, r.TwoByte} {
 		for _, b := range a.OpCode {
 			for _, c := range b.Entry {
@@ -80,10 +96,22 @@ func (r *xmlRef) Mnemonics() map[string]string {
 					if d.Mnem == "" {
 						continue
 					}
-					m[d.Mnem] = c.Brief
+					set[d.Mnem] = c.Brief
 				}
 			}
 		}
 	}
-	return m
+	return set
 }
+
+// A Mnemonic represents an x86/x86-64 mnemonic with its name and description brief.
+type Mnemonic struct {
+	Name  string
+	Brief string
+}
+
+type byName []Mnemonic
+
+func (s byName) Len() int           { return len(s) }
+func (s byName) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s byName) Less(i, j int) bool { return s[i].Name < s[j].Name }
